d341f2f35b5b69be67b9d763ab6a07fbbca3a514
changhuapeng <>
2024-06-12 01:41:01 +0800

    Add basic PIF fields logging

--- app/src/main/java/com/android/internal/util/framework/Android.java
+++ app/src/main/java/com/android/internal/util/framework/Android.java
@@ -128,16 +128,9 @@ public final class Android {
 
         if (!"com.google.android.gms.unstable".equals(processName)) return;
 
+        LOG("DroidGuard detected!");
         map.forEach((fieldName, value) -> {
-            Field field = getField(fieldName);
-            if (field == null) return;
-            field.setAccessible(true);
-            try {
-                field.set(null, value);
-            } catch (Throwable t) {
-                Log.e(TAG, t.toString());
-            }
-            field.setAccessible(false);
+            setField(fieldName, value);
         });
     }
 
@@ -221,5 +214,94 @@ public final class Android {
             Log.e(TAG, t.toString());
         }
         return caList;
+    }
+
+    private static boolean classContainsField(Class className, String fieldName) {
+        for (Field field : className.getDeclaredFields()) {
+            if (field.getName().equals(fieldName)) return true;
+        }
+        return false;
+    }
+
+    private static void setField(String name, String value) {
+        if (value.isEmpty()) {
+            LOG(String.format("%s is empty, skipping", name));
+            return;
+        }
+
+        Field field = null;
+        String oldValue = null;
+        Object newValue = null;
+
+        try {
+            if (classContainsField(Build.class, name)) {
+                field = Build.class.getDeclaredField(name);
+            } else if (classContainsField(Build.VERSION.class, name)) {
+                field = Build.VERSION.class.getDeclaredField(name);
+            } else {
+                LOG(String.format("Couldn't determine '%s' class name", name));
+                return;
+            }
+        } catch (NoSuchFieldException e) {
+            LOG(String.format("Couldn't find '%s' field name: " + e, name));
+            return;
+        }
+        field.setAccessible(true);
+        try {
+            oldValue = String.valueOf(field.get(null));
+        } catch (IllegalAccessException e) {
+            LOG(String.format("Couldn't access '%s' field value: " + e, name));
+            return;
+        }
+        if (value.equals(oldValue)) {
+            LOG(String.format("[%s]: %s (unchanged)", name, value));
+            return;
+        }
+        Class<?> fieldType = field.getType();
+        if (fieldType == String.class) {
+            newValue = value;
+        } else if (fieldType == int.class) {
+            newValue = Integer.parseInt(value);
+        } else if (fieldType == long.class) {
+            newValue = Long.parseLong(value);
+        } else if (fieldType == boolean.class) {
+            newValue = Boolean.parseBoolean(value);
+        } else {
+            LOG(String.format("Couldn't convert '%s' to '%s' type", value, fieldType));
+            return;
+        }
+        try {
+            field.set(null, newValue);
+        } catch (IllegalAccessException e) {
+            LOG(String.format("Couldn't modify '%s' field value: " + e, name));
+            return;
+        }
+        field.setAccessible(false);
+        LOG(String.format("[%s]: %s -> %s", name, oldValue, value));
+    }
+
+    private static String logParseField(Field field) {
+        Object value = null;
+        String type = field.getType().getName();
+        String name = field.getName();
+        try {
+            value = field.get(null);
+        } catch (IllegalAccessException|NullPointerException e) {
+            return String.format("Couldn't access '%s' field value: " + e, name);
+        }
+        return String.format("<%s> %s: %s", type, name, String.valueOf(value));
+    }
+
+    private static void logFields() {
+        for (Field field : Build.class.getDeclaredFields()) {
+            LOG("Build " + logParseField(field));
+        }
+        for (Field field : Build.VERSION.class.getDeclaredFields()) {
+            LOG("Build.VERSION " + logParseField(field));
+        }
+    }
+
+    static void LOG(String msg) {
+        Log.d("PIF/Java", msg);
     }
 }
